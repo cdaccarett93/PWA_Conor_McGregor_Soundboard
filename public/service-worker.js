da1b4d2fc6c1ddbbc4e3ac27ebc7d8edb4a589bb
Carlos Daccarett <cdaccarett93@gmail.com>
2017-10-12 10:28:35 -0700

    updated services worker to support offline usage

--- public/service-worker.js
+++ public/service-worker.js
@@ -32,9 +32,26 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.open('static-cache-v1.01').then(function(cache) {
-      return fetch(event.request).then(function(response) {
-        cache.put(event.request, response.clone());
-        return response;
+      return cache.match(event.request).then(function(response) {
+        //console.log("cache request: " + event.request.url);
+        var fetchPromise = fetch(event.request).then(
+          function(networkResponse) {
+            // if we got a response from the cache, update the cache
+            //console.log("fetch completed: " + event.request.url, networkResponse);
+            if (networkResponse) {
+              //console.debug("updated cached page: " + event.request.url, networkResponse);
+              cache.put(event.request, networkResponse.clone());
+            }
+            return networkResponse;
+          },
+          function(e) {
+            // rejected promise - just ignore it, we're offline
+            //console.log("Error in fetch()", e);
+          }
+        );
+
+        // respond from the cache, or the network
+        return response || fetchPromise;
       });
     })
   );
